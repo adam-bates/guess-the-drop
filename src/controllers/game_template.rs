c9982ad64be058608f7918d2c34957343ede1c20
adam-bates <adam@adambates.ca>
2023-11-19 01:53:20 -0500

    Starting to support creation of templates

--- src/controllers/game_template.rs
+++ src/controllers/game_template.rs
@@ -6,11 +6,26 @@ use crate::{
 };
 
 use askama::Template;
-use axum::{extract::State, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::{Query, State},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use nanoid::nanoid;
+use serde::Deserialize;
 use tower_sessions::Session;
 
+const MAX_TEMPLATES_PER_USER: usize = 100;
+
 pub fn add_routes(router: Router<AppState>) -> Router<AppState> {
-    return router.route("/game-templates", get(templates));
+    return router
+        .route("/game-templates", get(templates))
+        .route("/game-templates/new", get(new_template))
+        .route(
+            "/game-templates/new/x/add-item",
+            get(new_template_x_add_item),
+        );
 }
 
 #[derive(Template)]
@@ -18,20 +33,68 @@ pub fn add_routes(router: Router<AppState>) -> Router<AppState> {
 struct GameTemplatesTemplate {
     user: User,
     templates: Vec<GameTemplate>,
+    can_create: bool,
 }
 
 async fn templates(session: Session, State(state): State<AppState>) -> Result<impl IntoResponse> {
     let sid = utils::session_id(&session)?;
     let (user, _) = utils::require_user(&state, &sid).await?.split();
 
-    return Ok(GameTemplatesTemplate {
-        user,
-        templates: vec![GameTemplate {
-            game_template_id: 0,
-            user_id: 0,
+    let mut game_templates: Vec<GameTemplate> =
+        sqlx::query_as("SELECT * FROM game_templates WHERE user_id = ?")
+            .bind(&user.user_id)
+            .fetch_all(&state.db)
+            .await?;
 
-            name: "Zelda OOT Rando".to_string(),
+    let mut id = 1;
+
+    for _ in 0..20 {
+        game_templates.push(GameTemplate {
+            game_template_id: id,
+            user_id: user.user_id.clone(),
+
+            name: nanoid!(),
             reward_message: None,
-        }],
+        });
+
+        id += 1;
+    }
+
+    return Ok(GameTemplatesTemplate {
+        can_create: game_templates.len() < MAX_TEMPLATES_PER_USER,
+
+        user,
+        templates: game_templates,
     });
+}
+
+#[derive(Template)]
+#[template(path = "new-game-template.html")]
+struct NewGameTemplateTemplate {
+    user: User,
+}
+
+async fn new_template(
+    session: Session,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse> {
+    let sid = utils::session_id(&session)?;
+    let (user, _) = utils::require_user(&state, &sid).await?.split();
+
+    return Ok(NewGameTemplateTemplate { user });
+}
+
+#[derive(Deserialize)]
+struct NewGameTempateAddItemParams {
+    idx: u32,
+}
+
+#[derive(Template)]
+#[template(path = "new-game-template-add-item.html")]
+struct NewGameTemplateAddItemTemplate {
+    idx: u32,
+}
+
+async fn new_template_x_add_item(params: Query<NewGameTempateAddItemParams>) -> impl IntoResponse {
+    return NewGameTemplateAddItemTemplate { idx: params.idx };
 }
--- src/models/game_template.rs
+++ src/models/game_template.rs
@@ -8,7 +8,7 @@ use sqlx::{self, MySqlPool};
 #[derive(sqlx::FromRow, Serialize, Deserialize)]
 pub struct GameTemplate {
     pub game_template_id: u32,
-    pub user_id: u32,
+    pub user_id: String,
 
     pub name: String,
     pub reward_message: Option<String>,
